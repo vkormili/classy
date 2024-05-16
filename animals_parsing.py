90b6e250672ff9e00208cf58f4c05db367a3c1ef
Варвара Кормилицына <140538623+vkormili@users.noreply.github.com>
2024-05-17 01:05:59 +0300

    Update animals_parsing.py

--- animals_parsing.py
+++ animals_parsing.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import numpy as np
 
 
 def linkinator(anotherurl):  # часть 1, где объекты со страницы собираются в список, а мы пересматриваем финеса и ферба
@@ -28,15 +29,29 @@ def determinder(line, word):
 
 
 def classificator(link):
-    turtle = 'https://ru.wikipedia.org/wiki/%D0%90%D0%B2%D1%81%D1%82%D1%80%D0%B0%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B0' \
-              '%D1%8F_%D0%B7%D0%BC%D0%B5%D0%B8%D0%BD%D0%BE%D1%88%D0%B5%D1%8F%D1%8F_%D1%87%D0%B5%D1%80%D0%B5%D0%BF%D0' \
-              '%B0%D1%85%D0%B0 '
-    if link == turtle:
-        return 0
+    if link == 'https://ru.wikipedia.org/wiki/%D0%90%D1%80%D0%B0%D0%B2%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B0%D1%80%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2%D1%8B%D0%B9_%D0%B3%D0%B5%D0%BA%D0%BA%D0%BE%D0%BD_%D0%A1%D0%BA%D0%BE%D1%80%D1%82%D0%B5%D1%87%D1%87%D0%B8':
+        return ['Аравийский карликовый геккон Скортеччи', 'Тропиколоты', 'Гекконовые', 'Чешуйчатые', 'Пресмыкающиеся', 'Хордовые']
+    # print(link)
     name = pd.read_html(link, flavor='lxml')[0].columns.values[0]
-    print(name)
-    data = pd.read_html(link, flavor='lxml')[0][name][2]
-    if 'Вид:' not in data or '†' in data:
+    if type(name) == np.int64:
+        if 'Unnamed: 0' in pd.read_html(link, flavor='lxml', header=1, index_col=1)[name]:
+            data = pd.read_html(link, flavor='lxml', header=1, index_col=1)[name]['Unnamed: 0'][1]
+        else:
+            return [link, 0, 0, 0, 0, 0]
+    else:
+        data_0 = pd.read_html(link, flavor='lxml')[0][name]
+        i = 0
+        while i < len(data_0):
+            if type(data_0[i]) != float:
+                if 'Домен:ЭукариотыЦарство:Животные' in data_0[i]:
+                    break
+            i += 1
+        if i >= len(data_0):
+            return [1, 1, 1, 1, 1, 1]
+        data = data_0[i]
+    if type(data) == float:
+        return [link, 0, 0, 0, 0, 0]
+    elif 'Вид:' not in data or '†' in data:
         return [0, 0, 0, 0, 0, 0]
     species = data.split('Вид:')[1]
     genus = determinder(data, 'Род:')
@@ -46,19 +61,13 @@ def classificator(link):
     phylum = determinder(data, 'Тип:')
     return [species, genus, family, order, class_y, phylum]
 
-
-# print(classificator('https://ru.wikipedia.org/wiki/%D0%90%D0%B2%D1%81%D1%82%D1%80%D0%B0%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9_%D0%BE%D1%80%D0%BB%D1%8F%D0%BA'))
-# print(classificator('https://ru.wikipedia.org/wiki/%D0%90%D0%B0%D1%80%D0%B4%D0%BE%D0%BD%D0%B8%D0%BA%D1%81'))
-# print(classificator('https://ru.wikipedia.org/wiki/%D0%90%D0%B2%D1%80%D0%BE%D1%80%D1%8B'))
-# print(classificator('https://ru.wikipedia.org/wiki/%D0%90%D0%B2%D1%81%D1%82%D1%80%D0%B0%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B0%D1%8F_%D0%BA%D0%BE%D1%80%D0%B0%D0%BB%D0%BB%D0%BE%D0%B2%D0%B0%D1%8F_%D0%BA%D0%BE%D1%88%D0%B0%D1%87%D1%8C%D1%8F_%D0%B0%D0%BA%D1%83%D0%BB%D0%B0'))
-
 url = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8' \
       '%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&from=%D0%90 '
 
 animals = pd.DataFrame(columns=['species', 'genus', 'family', 'order', 'class_y', 'phylum'])
 while url != 'stop':
     linkslist = linkinator(url)
-    for i in range(200):
+    for i in range(len(linkslist) - 1):
         animals.append(classificator('https://ru.wikipedia.org' + linkslist[i]))
     url = 'https://ru.wikipedia.org' + linkslist[-1]
     if url == 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0' \
