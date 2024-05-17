d00c4129c59af3dbfcfa1652e44235ca87b037d5
Варвара Кормилицына <140538623+vkormili@users.noreply.github.com>
2024-05-17 15:06:06 +0300

    Update animals_parsing.py

--- animals_parsing.py
+++ animals_parsing.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import numpy as np
+import csv
 
 
 def linkinator(anotherurl):  # часть 1, где объекты со страницы собираются в список, а мы пересматриваем финеса и ферба
@@ -22,16 +23,19 @@ def determinder(line, word):
     line_check = list(line.split(word)[1])
     j = 1
     answer = line_check[0]
-    while ord(line_check[j]) >= 1072:
-        answer += line_check[j]
-        j += 1
+    if 64 < ord(answer) < 91:
+        while 123 > ord(line_check[j]) > 96:
+            answer += line_check[j]
+            j += 1
+    else:
+        while ord(line_check[j]) >= 1072:
+            answer += line_check[j]
+            j += 1
     return answer
 
 
 def classificator(link):
-    if link == 'https://ru.wikipedia.org/wiki/%D0%90%D1%80%D0%B0%D0%B2%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B0%D1%80%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2%D1%8B%D0%B9_%D0%B3%D0%B5%D0%BA%D0%BA%D0%BE%D0%BD_%D0%A1%D0%BA%D0%BE%D1%80%D1%82%D0%B5%D1%87%D1%87%D0%B8':
-        return ['Аравийский карликовый геккон Скортеччи', 'Тропиколоты', 'Гекконовые', 'Чешуйчатые', 'Пресмыкающиеся', 'Хордовые']
-    # print(link)
+    print(link)
     name = pd.read_html(link, flavor='lxml')[0].columns.values[0]
     if type(name) == np.int64:
         if 'Unnamed: 0' in pd.read_html(link, flavor='lxml', header=1, index_col=1)[name]:
@@ -40,39 +44,61 @@ def classificator(link):
             return [link, 0, 0, 0, 0, 0]
     else:
         data_0 = pd.read_html(link, flavor='lxml')[0][name]
-        i = 0
-        while i < len(data_0):
-            if type(data_0[i]) != float:
-                if 'Домен:ЭукариотыЦарство:Животные' in data_0[i]:
+        j = 0
+        while j < len(data_0):
+            if type(data_0[j]) != float:
+                if 'Домен:ЭукариотыЦарство:Животные' in data_0[j]:
                     break
-            i += 1
-        if i >= len(data_0):
-            return [1, 1, 1, 1, 1, 1]
-        data = data_0[i]
+            j += 1
+        if j >= len(data_0):
+            return [0, 0, 0, 0, 0, 0]
+        data = data_0[j]
     if type(data) == float:
         return [link, 0, 0, 0, 0, 0]
     elif 'Вид:' not in data or '†' in data:
         return [0, 0, 0, 0, 0, 0]
     species = data.split('Вид:')[1]
-    genus = determinder(data, 'Род:')
-    family = determinder(data, 'Семейство:')
-    order = determinder(data, 'Отряд:')
-    class_y = determinder(data, 'Класс:')
-    phylum = determinder(data, 'Тип:')
+
+    if 'Род:' in data:
+        genus = determinder(data, 'Род:')
+    else:
+        genus = 0
+    if '' in data:
+        family = determinder(data, 'Семейство:')
+    else:
+        family = 0
+    if '' in data:
+        order = determinder(data, 'Отряд:')
+    else:
+        order = 0
+    if '' in data:
+        class_y = determinder(data, 'Класс:')
+    else:
+        class_y = 0
+    if '' in data:
+        phylum = determinder(data, 'Тип:')
+    else:
+        phylum = 0
     return [species, genus, family, order, class_y, phylum]
+
+
+# print(classificator('https://ru.wikipedia.org/wiki/%D0%91%D0%B0%D0%BD%D0%B0%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BB%D0%B8%D1%81%D1%82%D0%BE%D0%BD%D0%BE%D1%81'))
 
 url = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8' \
       '%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&from=%D0%90 '
 
-animals = pd.DataFrame(columns=['species', 'genus', 'family', 'order', 'class_y', 'phylum'])
-while url != 'stop':
+flag = True
+
+f = open('animals.csv', 'w', encoding='utf-8')
+
+while flag:
     linkslist = linkinator(url)
     for i in range(len(linkslist) - 1):
-        animals.append(classificator('https://ru.wikipedia.org' + linkslist[i]))
+        csv.writer(f).writerow(classificator('https://ru.wikipedia.org' + linkslist[i]))
+        if 'https://ru.wikipedia.org/wiki/%D0%AF%D1%89%D1%83%D1%80%D0%BA%D0%B8' == 'https://ru.wikipedia.org' + \
+                linkslist[i]:
+            flag = False
+            break
     url = 'https://ru.wikipedia.org' + linkslist[-1]
-    if url == 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0' \
-              '%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82' \
-              '%D1%83&from=%D0%AF%D1%8E':
-        break
 
-animals.to_csv()
+f.close()
