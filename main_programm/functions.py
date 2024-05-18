ed245100c5b5aa74d1c4df4ba0efdd98c41012f0
PolinaHitrun <phitrun@icloud.com>
2024-05-18 11:32:25 +0300

    clean code

--- main_programm/functions.py
+++ main_programm/functions.py
@@ -1,38 +1,13 @@
 import random
 import csv
 
-# здесь можно все эти тройки прописывать в одной функции, но я не понимаю как из интерфейса вытащить что-то в переменную
-# везде две версии, общая и для каждого мода отдельно
 
 anim_base = open('probe.csv', 'r', newline='', encoding='utf-8')
 lang_base = open('probe.csv', 'r', newline='', encoding='utf-8')
 plant_base = open('probe.csv', 'r', newline='', encoding='utf-8')
 
-# эти инпуты нужно достать из ui
 
-# mode = 'anim'
-# userguess = 'Австралийская пятнистая макрель'
-
-# загадываем что-то рандомом
-
-
-# def ask_anim():
-#     ask = random.choice(anim_base.readlines()).rstrip().split(';')
-#     return ask
-#
-#
-# def ask_lang():
-#     ask = random.choice(lang_base.readlines()).rstrip().split(';')
-#     return ask
-#
-#
-# def ask_plant():
-#     ask = random.choice(plant_base.readlines()).rstrip().split(';')
-#     return ask
-
-
-# это если мы можем вытащить мод в переменную сразу при выборе режима
-
+# загадывает правильный ответ
 def asking(mode):
     if mode == 'anim':
         base = anim_base
@@ -45,31 +20,6 @@ def asking(mode):
 
 
 # достаем характеристику вида/языка из таблицы
-
-# def guess_anim(userguess):
-#     res = 0
-#     for row in csv.reader(open('probe.csv')):
-#         r = row[0].split(';')
-#         if userguess == r[0]:
-#             res = r
-#     return res
-#
-# def guess_plant():
-#     res = 0
-#     for row in csv.reader(open('файл с растениями')):
-#         r = row[0].split(';')
-#         if userguess == r[0]:
-#             res = r
-#     return res
-#
-# def guess_lang():
-#     res = 0
-#     for row in csv.reader(open('файл с языками')):
-#         r = row[0].split(';')
-#         if userguess == r[0]:
-#             res = r
-#     return res
-
 def guess(userguess, mode):
     if mode == 'anim':
         base = anim_base
@@ -85,20 +35,9 @@ def guess(userguess, mode):
     return res
 
 
-# списки для сравнения генеалогии,
-# записываем один раз при выборе режима
-# a_anim = ask_anim()
-# перезаписываем при каждом заполнении строки
-# g_anim = guess_anim()
-
-def proxi(a, g, mode):
+def proxi(a: list, g:str, mode) -> type(int, str):
+    # функция проверяет, насколько близко пользователь угадал
     proximity = 0
-    # if mode == 'anim':
-    #     base = anim_base
-    # if mode == 'plant':
-    #     base = plant_base
-    # if mode == 'lang':
-    #     base = lang_base
     anim_base = open('probe.csv', 'r', newline='', encoding='utf-8')
     reader = csv.reader(anim_base, delimiter=';')
     for row in reader:
--- main_programm/main.py
+++ main_programm/main.py
@@ -7,6 +7,7 @@ import functions
 
 
 def get_list_items(filename: str) -> list:
+    # получает список всех единиц из csv файла
     f = open(filename, 'r', newline='', encoding='utf-8')
     reader = csv.reader(f, delimiter=';')
     lst = []
@@ -89,37 +90,47 @@ class Game(QWidget):
             self.levels.addWidget(vid_label)
 
             self.guess_status.setText(self.guess_status.text() + ' царство')
-        # elif self.mode == 'lang':
-        #     self.lineEdit.insert('языка')
 
+        elif self.mode == 'lang':
+            self.lineEdit.insert('языка')
+            self.progressBar.setMaximum(0)
+
+        # подключение действия для кнопки на главную страницу
         self.back_btn.clicked.connect(self.back)
 
+        # устанавливаем значения для шкалы прогресса
         self.progressBar.setMinimum(0)
         self.progressBar.setValue(0)
-
+        # подключаем поисковую строку
         self.lineEdit.textEdited.connect(self.searching)
 
     def check(self):
+        # действие проверки ответа от пользователя
         user_guess = self.sender().text()
         self.textBrowser.insertPlainText(user_guess + '\n')
-        print(self.answer)
-        print(user_guess)
+        # сверяем ответ с правильным
         ball, level = functions.proxi(self.answer, user_guess, self.mode)
         if self.progressBar.value() < ball:
+            # если продвинулся, то отображаем на экране
             self.guess_status.setText('Угадан уровень генеалогии: ' + level)
             self.progressBar.setValue(ball)
+            # если угадал
             if self.progressBar.value() == self.progressBar.maximum():
                 self.guess_status.setText('ура! ты победил(а)!')
 
 
     def back(self):
+        # возвращение на главную
         ex.show()
         self.close()
 
     def searching(self):
+        # функционал поисковой строки
+        # очистка предыдущих запросов
         while self.search.count():
             widget = self.search.takeAt(0).widget()
             self.search.removeWidget(widget)
+        # поиск регулярными выражениями по запросу
         to_find = self.lineEdit.text()
         all_items = get_list_items('probe.csv')
         results = []
@@ -128,7 +139,8 @@ class Game(QWidget):
                 results.append(re.fullmatch(f'.*{to_find}.*', item)[0])
             except TypeError:
                 pass
-        for result in results[:8]:
+        # вывод некоторых результатов на экран
+        for result in results[:15]:
             btn_search = QPushButton(result)
             btn_search.clicked.connect(self.check)
             self.search.addWidget(btn_search)
